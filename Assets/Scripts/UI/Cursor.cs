88fadb8001abf601c06aca71758d6d0f4ae5fc83
unknown <alex.mulford@gmail.com>
2019-09-22 17:33:00 +0100

    adding drag cursors

--- Assets/Scripts/UI/Cursor.cs
+++ Assets/Scripts/UI/Cursor.cs
@@ -8,6 +8,13 @@ public class GameCursor
     private Transform _cursor;
     private SpriteRenderer _spriteRenderer;
 
+    private Vector3 _min;
+    private Vector3 _max;
+    private bool _mouseDown;
+
+    private Vector2Int _minVec2Int { get { return _min.ToVector2IntFloor(); } }
+    private Vector2Int _maxVec2Int { get { return _max.ToVector2IntFloor(); } }
+
     public GameCursor(Game game)
     {
         _game = game;
@@ -34,9 +41,25 @@ public class GameCursor
         var position = new Vector3(
             Mathf.RoundToInt(mousePosition.x), 
             Mathf.RoundToInt(mousePosition.y)
-        );
+        );  
 
         _cursor.transform.position = position;
+
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            _mouseDown = true;
+            _min = _cursor.transform.position;
+        }
+        else if(Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            _mouseDown = false;
+        }
+
+        if(_mouseDown)
+        {
+            _max = _cursor.transform.position;
+        }
+
     }
 
     public void DrawGizmos()
@@ -49,6 +72,15 @@ public class GameCursor
             Mathf.RoundToInt(mousePosition.x), 
             Mathf.RoundToInt(mousePosition.y)
         );
+
+        if(_mouseDown)
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireCube(_min + (_max - _min) / 2, _max - _min);
+        
+            Gizmos.DrawWireSphere(_minVec2Int.ToVector3(), 0.2f);
+            Gizmos.DrawWireSphere(_maxVec2Int.ToVector3(), 0.2f);
+        }
 
         Gizmos.color = Color.green;
         Gizmos.DrawWireCube(position, Vector3.one);
