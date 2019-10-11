2cd1d89f17c3ed128cae0a826c10454b344addd2
unknown <alex.mulford@gmail.com>
2019-10-11 16:35:30 +0100

    update before change

--- Assets/Scripts/AI/Villager.cs
+++ Assets/Scripts/AI/Villager.cs
@@ -179,7 +179,6 @@ public class Villager : ThingAgent
             Effects         = { { "hasThing", TypeOfThing.Axe }, { "hasFullInventory", true }, { "isWorking", true } }
         });
 
-
         /*
             Construction
         */
