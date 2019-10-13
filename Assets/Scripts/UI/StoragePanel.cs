71ef8c2e3611f23a073e2a501343225b0752e652
unknown <alex.mulford@gmail.com>
2019-10-13 18:50:26 +0100

    added storage container

--- Assets/Scripts/UI/StoragePanel.cs
+++ Assets/Scripts/UI/StoragePanel.cs
@@ -1,11 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
 
 public class StoragePanel : MonoBehaviour
 {
+    public Game Game;
+    public ObjectPooler TogglePooler;
+
     public void Setup(Thing thing)
     {
+        TogglePooler.DeactivateAll();
+
+        foreach(var item in Game.AllThings.Where(t => t.resource))
+        {
+            var obj = TogglePooler.GetPooledObject();
+            obj.GetComponentInChildren<Text>().text = item.name.ToUppercaseFirst();
+            var toggle = obj.GetComponentInChildren<Toggle>();
+            toggle.onValueChanged.RemoveAllListeners();
+            toggle.onValueChanged.AddListener((value) => {
+                if(value) 
+                    thing.storage.Allow(item.type);
+                else
+                    thing.storage.Disallow(item.type);
+            });
+        }
+
         gameObject.SetActive(true);
     }
 }
