1180862e9e73d2ebba1770c7fccd5384b637e453
unknown <alex.mulford@gmail.com>
2019-11-19 13:55:52 +0000

    update

--- Assets/Scripts/Enums/TypeOfThing.cs
+++ Assets/Scripts/Enums/TypeOfThing.cs
@@ -58,6 +58,7 @@
     CarpentersBench,
     Axe                     ,
     Hoe                     ,
+    Pickaxe,
     Storage                 ,
     // StorageBlueprint        ,
     Tree                    ,
